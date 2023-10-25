7ce182c0e480df3b15c519529d018650421d2c30
Joaovitorufu <joaovitorufu@gmail.com>
2023-10-25 10:12:57 -0300

    Começando a popular o tabuleiro

--- tabuleiro/Tabuleiro.cs
+++ tabuleiro/Tabuleiro.cs
@@ -21,6 +21,14 @@ namespace tabuleiro
         {
             return Pecas[linha, coluna];
         }
+
+        public void colocarPeca(Peca p, Posicao pos)
+        {
+            Pecas[pos.Linha,pos.Coluna] = p;
+            p.Posicao = pos;
+        }
+
+
     }
 
     
